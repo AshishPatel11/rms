d9b6659b4d49a44b0f876e7ddde27156f1dc8986
Ashish Patel <ashishpatel287680@gmail.com>
2023-05-02 14:34:41 +0530

    commit

--- src/components/login.js
+++ src/components/login.js
@@ -3,7 +3,7 @@ function login() {
         <div className="loginForm">
             <form className="form">
                 <div className="input-fields">
-                    {/* <span className="material-symbols-outlined">account_circle</span> */}
+                    <span className="material-symbols-outlined">account_circle</span>
                     <input type="text" className="form-text" placeholder="Enter Username" />
                 </div>
                 <input type="submit" className="form-btn" value="Verify"/>
