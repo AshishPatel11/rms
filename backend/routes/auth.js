25293b73e550c2a936333e26673520b43c3069c3
Ashish Patel <ashishpatel287680@gmail.com>
2023-05-06 12:56:04 +0530

    lab commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-// const { query, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'ashishpatel';
+let rand = function () {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+let token = function () {
+    return rand() + rand() + rand(); // to make it longer
+};
 
+token();
+// const { query, validationResult } = require('express-validator');
 router.post('/login', async (req, res) => {
     const { userName } = req.body;
     try {
-        let user = User.finfOne({ userName })
+        let user = await User.findOne({ username: userName })
         if (!user) {
-            return res.send(400).json({error: "user not found"})
+            return res.send(400).json({ error: "user not found" })
         }
-        res.send("hello");
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        return res.json(authtoken);
     } catch (error) {
 
     }
