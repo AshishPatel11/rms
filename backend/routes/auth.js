b0fd5a068f6c4fdaef4feac326e23f77f71be5cc
AShish Patel <ashishpatel287680@gmail.com>
2023-05-28 12:37:51 +0530

    DONE

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+// const app = express();
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
