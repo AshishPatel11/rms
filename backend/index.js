2ea86adb01851ac4da0464be61ea250121b6e255
AShish Patel <ashishpatel287680@gmail.com>
2023-06-05 10:28:52 +0530

    commit done

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,54 @@
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { GridFsStorage } = require("multer-gridfs-storage");
+require("dotenv").config();
 connectToMongo();
+process.on('unhandledRejection', error => {
+    console.log('unhandledRejection', error.message);
+});
 const app = express()
 const port = 5000
 app.use(express.json());
-app.use('/api/auth',require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
+//creating bucket
+let bucket;
+mongoose.connection.on("connected", () => {
+    var db = mongoose.connections[0].db;
+    bucket = new mongoose.mongo.GridFSBucket(db, {
+        bucketName: "MCA.DS"
+    });
+});
 
+//to parse json content
+app.use(express.json());
+//to parse body from url
+app.use(express.urlencoded({
+    extended: false
+}));
+const storage = new GridFsStorage({
+    url: "mongodb://127.0.0.1/RMS",
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            const filename = file.originalname;
+            const fileInfo = {
+                filename: filename,
+                bucketName: "MCA.DS"
+            };
+            resolve(fileInfo);
+        });
+    }
+});
+
+const upload = multer({
+    storage
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200)
+        .send("File uploaded successfully");
+});
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
 })
--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,46 @@
-function login() {
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+
+const Login = (props) => {
+    const [credentials, setCredentials] = useState({ email: "", password: "" })
+    let history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success) {
+            // Save the auth token and redirect
+            localStorage.setItem('token', json.authtoken);
+            history.push("/");
+
+        }
+        else {
+            alert("Invalid credentials");
+        }
+    }
+
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
     return (
         <div className="loginForm">
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <div className="input-fields">
                     <span className="material-symbols-outlined">account_circle</span>
-                    <input type="text" className="form-text" placeholder="Enter Username" />
+                    <input type="text" className="form-text" onChange={onChange} placeholder="Enter Username" />
                 </div>
-                <input type="submit" className="form-btn" value="Verify"/>
+                <input type="submit" className="form-btn" value="Verify" />
             </form>
         </div>
-    );
+    )
 }
-export default login;
+export default Login;
